ce76f58bf0a6f7c9bcffa3e897d2b47b8fa27cd3
DerevenetsArtyom <artem@comvoy.be>
2018-10-02 20:25:48 +0300

    Add comments to steps

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def lucas():
         a, b = b, a + b
 
 
-# Regular function: directly returns the result or raises an exception
+# 1) Regular function: directly returns the result or raises an exception
 def search(iterable, predicate):
     for item in iterable:
         if predicate(item):
@@ -19,9 +19,18 @@ search(lucas(), lambda x: len(str(x)) >= 6)
 # >>> 103682
 
 
+# 2) Periodically yields control to scheduler
 def async_search(iterable, predicate):
     for item in iterable:
         if predicate(item):
             return item
         yield
     raise ValueError("Not Found")
+
+
+g = async_search(lucas(), lambda x: x >= 10)
+# <generator object async_search at 0x7f2b4aac8200>
+# actually encapsulate code and state of that code / function
+# >>> next(g)
+# >>> next(g)
+# >>> next(g)
