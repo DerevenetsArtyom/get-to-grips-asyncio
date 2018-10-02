541a425d6e3425d198d808df8cfc2b8e554fc05c
DerevenetsArtyom <artem@comvoy.be>
2018-10-02 22:05:07 +0300

    Create second version 'async_repetitive_message'

--- repetitive_message_evolution.py
+++ repetitive_message_evolution.py
@@ -3,6 +3,18 @@ import time
 
 # 5) Create function that print a message and sleeps actually for some time
 def repetitive_message(message, interval_seconds):
+    while True:
+        print(message)
+        start = time.time()
+        expiry = start + interval_seconds
+        while True:
+            now = time.time()
+            if now >= expiry:
+                break
+
+
+# 6) Refactor that to make it cooperative (return control)
+def async_repetitive_message(message, interval_seconds):
     """Yields control until time interval expires"""
     while True:
         print(message)
@@ -12,3 +24,4 @@ def repetitive_message(message, interval_seconds):
             now = time.time()
             if now >= expiry:
                 break
+            yield
