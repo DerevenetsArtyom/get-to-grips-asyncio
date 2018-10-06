fac8f27bf3ae304ae219f74d63762a48670cc2d6
DerevenetsArtyom <artem@comvoy.be>
2018-10-06 22:15:30 +0300

    Add version with 'ensure_future' function

--- with_asyncio/tasks.py
+++ with_asyncio/tasks.py
@@ -1,22 +1,30 @@
 import asyncio
+from asyncio import ensure_future
 from main import lucas, is_prime, search
-from futures import twelve_digits_prime
+from futures import twelve_digits_prime, monitor_future
 
 # TASK - a subclass of Future which wraps a coroutine (wtf?)
 # Task is-a Future
+# Task has/contains a coroutine
 
+# #1. Version with 'create_task' function
 
-async def monitored_search(iterable, predicate, future):
-    try:
-        found_item = await search(iterable, predicate)
-    except ValueError as not_found:
-        future.set_exeption(not_found)
-    else:  # no exception
-        future.set_result(found_item)
+# loop = asyncio.get_event_loop()
+# co_obj = search(lucas(), twelve_digits_prime)
+# search_task = loop.create_task(co_obj)
+# loop.create_task(monitor_future(search_task, 1))
+# loop.run_until_complete(search_task)
+# print(search_task.result())
+# loop.close()
 
+# #2. Version with 'ensure_future' function
+if __name__ == '__main__':
+    search_task = ensure_future(search(lucas(), twelve_digits_prime))
+    monitor_task = ensure_future(monitor_future(search_task, 1))
 
-async def monitor_future(future, interval_seconds):
-    while not future.done():
-        print('Waiting...')
-        await asyncio.sleep(interval_seconds)
-    print('Done!')
+    search_and_monitor_future = asyncio.gather(search_task, monitor_task)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(search_and_monitor_future)
+    print(search_task.result())
+    loop.close()
